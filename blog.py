e1f72f501b78c6a58838fb04362707d4f3516c08
BroccoliRob <rschn@uga.edu>
2014-07-22 18:28:30 -0400

    added login/logout functionality

--- blog.py
+++ blog.py
@@ -5,24 +5,45 @@ from flask import Flask, render_template, request, session, \
 import sqlite3
 
 # configuration
-DATABASE= 'blog.db'
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'd-9kghxa*9w+lpwp6eyl@zy8$n1w7o7=zla(ibxc%yzzb8v$d3'
 
 app = Flask(__name__)
 
+
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
+
 
 # function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials.Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
